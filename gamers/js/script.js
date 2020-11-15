da8ad001d491e9ecbdd94d2c7294663bcbb77779
Edentor <dmitriiguliaev@gmail.com>
2020-11-15 16:23:03 +0300

    add button

---
+++ gamers/js/script.js
@@ -0,0 +1,5 @@
+const buttons = document.querySelectorAll('#popup-button');
+const modal = document.querySelector('.modal-wrapper')
+buttons.forEach(button => {
+    button.addEventListener('click', () => modal.classList.toggle('open'))
+})
