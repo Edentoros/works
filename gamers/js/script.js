c3a8d6346f8d6eb84510bd285054f5c6031766ac
Edentor <dmitriiguliaev@gmail.com>
2020-11-15 20:23:10 +0300

    add respnsove

--- gamers/js/script.js
+++ gamers/js/script.js
@@ -1,5 +1,5 @@
 const buttons = document.querySelectorAll('#popup-button');
-const modal = document.querySelector('.modal-wrapper')
+const modal = document.querySelector('.modal-wrapper');
 buttons.forEach(button => {
     button.addEventListener('click', () => modal.classList.toggle('open'))
-})
+});
