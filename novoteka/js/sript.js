c8e9d6ac85a64c8f3fb815a368c194cc78854afb
Edentor <dmitriiguliaev@gmail.com>
2020-11-16 16:02:59 +0300

    feat: add novoteka

---
+++ novoteka/js/sript.js
@@ -0,0 +1,27 @@
+const search = document.querySelector('.nav__search')
+const navbar = document.querySelector('.nav')
+const searchBar = document.querySelector('.nav__search-input')
+
+
+search.addEventListener('click', () => {
+        search.classList.add('visible');
+        searchBar.onfocus = function () {
+
+            document.body.style.transitionDuration = '0.5s';
+            document.body.style.opacity = 0.7;
+
+
+        }
+        searchBar.focus();
+
+    },
+    searchBar.onblur = function () {
+        search.classList.remove('visible');
+
+        document.body.style.transitionDuration = '0.5s';
+        document.body.style.opacity = 1;
+
+
+
+    }
+)
