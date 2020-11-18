bed4124b250a7be66365b6e26ebdec0abbea5bca
Edentor <dmitriiguliaev@gmail.com>
2020-11-18 12:10:53 +0300

    fix: remove additional useless folders

--- gamers/js/script.js
+++
@@ -1,5 +0,0 @@
-const buttons = document.querySelectorAll('#popup-button');
-const modal = document.querySelector('.modal-wrapper');
-buttons.forEach(button => {
-    button.addEventListener('click', () => modal.classList.toggle('open'))
-});
